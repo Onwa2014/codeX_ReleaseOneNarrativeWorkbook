f22322e2ba4c49e068c8c5bf0a0c78bb04a20423
Onwaba <onwaba@projectcodex.co>
2015-03-12 16:22:03 +0200

    Node_tutorial

--- node-tutorial/users.js
+++ node-tutorial/users.js
@@ -1,5 +1,8 @@
 // a empty user list
 var users = [];
+users.push({first_name:"Jack",last_name:"Bradshaw"});
+users.push({first_name:"Callum",last_name:"Bradshaw"});
+users.push({first_name:"Neil",last_name:"Bradshaw"});
 
 //create 3 users in the users array
 //using code like this: users.push({ first_name : 'Andy', last_name : 'Bradshaw' });
@@ -8,5 +11,8 @@ var users = [];
 
 //now loop through the users array and write the following data to the console below each other for each user
 //you can use the forEach method
-
+for(var i = 0; i < users.length; i++){
+	//console.log(users[i]);
+	console.log("Hi,"+" "+ users[i].first_name + " " + users[i].last_name +"!");
+}
 //Hi, first_name last_name!
